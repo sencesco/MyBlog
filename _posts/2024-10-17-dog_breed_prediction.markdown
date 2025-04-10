3c8a09246746f93f57809077b07de7420c53f403
Somchai Kradingthong <somchai.kdt@gmail.com>
2025-04-10 21:07:45 +0700

    fix:dog breed prediction link

--- _posts/2024-10-17-dog_breed_prediction.markdown
+++ _posts/2024-10-17-dog_breed_prediction.markdown
@@ -7,8 +7,8 @@ date: 2024-10-17
 categories: ["ML","DL","Computer Vision", "Python"]
 image: /assets/image/post_image/dog_breed_1.png
 read_time: 6
-github-repo: https://github.com/sencesco/Artificial-Intelligence/blob/main/Dog%20Breed%20Prediction%20(50%20class%20prediction)/Kaggle%20notebook%20Code/dog-breed-prediction.ipynb
-colab-link: https://colab.research.google.com/github/sencesco/Artificial-Intelligence/blob/main/Dog%20Breed%20Prediction%20(50%20class%20prediction)/Kaggle%20notebook%20Code/dog-breed-prediction.ipynb
+github-repo: https://github.com/sencesco/Deep-Learning/blob/main/dog-breed-prediction.ipynb
+colab-link: https://colab.research.google.com/github/sencesco/Deep-Learning/blob/main/dog-breed-prediction.ipynb
 ---
 
 <div id="c-s-hlist">
@@ -28,7 +28,7 @@ colab-link: https://colab.research.google.com/github/sencesco/Artificial-Intelli
 {{ site.tabc }} In this challenge, I using a deep learning model to predict dog breeds from images. This focuses on building a classifier that can predict 50 dog breeds classes using convolutional neural networks (CNNs). The actual dataset is 120 classes but for faster and computational limitations in training I decided to use 50 classes. And using <span class="post-content-link"><a href="https://www.ibm.com/topics/transfer-learning" target="_blank">transfer learning</a></span> with a pretrained model named's MobileNetV2 from Keras. I used <span class="post-content-link"><a href="https://www.kaggle.com/datasets/catherinehorng/dogbreedidfromcomp" target="_blank">Kaggle's Dog Breed Identification dataset</a></span> for this challenge, and the model was built using TensorFlow/Keras.
 
 <div id="notebook-content">
-    https://raw.githubusercontent.com/sencesco/Artificial-Intelligence/refs/heads/main/Dog%20Breed%20Prediction%20(50%20class%20prediction)/Kaggle%20notebook%20Code/dog-breed-prediction.ipynb
+    https://raw.githubusercontent.com/sencesco/Deep-Learning/refs/heads/main/dog-breed-prediction.ipynb
 </div>
 <script id="notebook-filters-ignore" type="application/json"> 
     {
@@ -70,7 +70,7 @@ colab-link: https://colab.research.google.com/github/sencesco/Artificial-Intelli
 </script>
 
 ## Closing words
-{{ site.tabc }} From my experience with this challenge, I found the weakness thing when again future data is being used. That I chose MobileNetV2 from I mention before but in another reason for concern about model size that this architecture will lightweight size of model. But for the image classification problem, some architectures will be more suitable for this dataset, like EfficientNet, ResNet,VGG16/VGG19 and etc., that will use a larger model size than MobileNetV2. You could see more information about architecture in <span class="post-content-link"><a href="https://keras.io/api/applications/" target="_blank">Keras Applications</a></span>.
+{{ site.tabc }} From my experience with this challenge, I found the weakness thing when again future data is being used. That I chose MobileNetV2 from I mention before but in another reason for concern about model size that this architecture will lightweight size of model. But for the image classification problem, some architectures will be more suitable for this dataset, For example: EfficientNet, ResNet,VGG16/VGG19 and etc., that will use a larger model size than MobileNetV2. You could see more information about architecture in <span class="post-content-link"><a href="https://keras.io/api/applications/" target="_blank">Keras Applications</a></span>. And add some pre-processing for example: Contrast enhancement, histogram equalization, and so on. Can help a model perform better.
 
 {{ site.tabc }} So in the real world, considering a matching dataset with a suitable architecture will make it perform better. And if we can use a lighter model, it will be better, but we need to consider the characteristics of architecture first for model performance.
 
--- assets/rb/calculate_read_time.rb
+++ assets/rb/calculate_read_time.rb
@@ -1,105 +1,62 @@
-require 'fileutils'
-require 'net/http'
 require 'json'
 
-# Function to calculate the read time
-def calculate_read_time(content, words_per_minute = 200)
-  word_count = content.split.size
-  (word_count.to_f / words_per_minute).ceil
-end
+def fetch_notebook_content(notebook_path)
+  markdown_content = ''
 
+  begin
+    file = File.read(notebook_path)
+    json = JSON.parse(file)
 
-# Function to fetch content from a URL
-def fetch_content_from_url(url)
-  uri = URI(url)
-  response = Net::HTTP.get(uri)
-  response  # Return the raw content
-rescue => e
-  puts "Error fetching content from URL: #{url}, Error: #{e.message}"
-  return ""
-end
+    cells = json['cells'] || []
 
+    cells.each do |cell|
+      # Skip unwanted cells by metadata
+      next if cell.dig('metadata', 'tags')&.include?('skip_read_time')
 
-# Function to fetch and process the notebook content
-def fetch_notebook_content(url)
-  content = fetch_content_from_url(url)
-  notebook = JSON.parse(content)
-  markdown_content = ""
-  code_content = ""
-  # Iterate through notebook cells and gather markdown and code
-  notebook['cells'].each do |cell|
-    if cell['cell_type'] == 'markdown'
-      markdown_content += cell['source'].join(' ')  # Join all markdown lines
-    elsif cell['cell_type'] == 'code'
-      code_content += cell['source'].join(' ')  # Join all code lines
+      source = cell['source'].is_a?(Array) ? cell['source'].join(' ') : cell['source'].to_s
+
+      if cell['cell_type'] == 'markdown'
+        markdown_content += source
+      elsif cell['cell_type'] == 'code'
+        # Optionally include code in read time calculation
+        markdown_content += source
+      end
     end
+
+  rescue => e
+    puts "Error reading notebook: #{notebook_path}"
+    puts e.message
   end
-  # Combine markdown and code content
-  markdown_content + code_content
+
+  markdown_content
 end
 
-
-# Function to fetch .py file content
-def fetch_python_file_content(url)
-  fetch_content_from_url(url)
+def calculate_read_time(text)
+  words_per_minute = 200.0
+  word_count = text.split.size
+  (word_count / words_per_minute).ceil
 end
 
-
-# Process the markdown file and include external notebook and .py content
 def process_markdown_file(file_path)
-  # Read the content of the markdown file
   content = File.read(file_path)
-  combined_content = content
-  # Fetch the notebook content if a URL exists
-  notebook_url = extract_notebook_url(content)
-  if notebook_url
-    external_notebook_content = fetch_notebook_content(notebook_url)
-    combined_content += external_notebook_content
+
+  notebook_path_match = content.match(/notebook_path:\s*(.*\.ipynb)/)
+
+  if notebook_path_match
+    notebook_path = notebook_path_match[1].strip
+    notebook_full_path = File.expand_path("../#{notebook_path}", __dir__)
+    notebook_text = fetch_notebook_content(notebook_full_path)
+    total_text = content + "\n" + notebook_text
+  else
+    total_text = content
   end
 
-  # Fetch the Python (.py) files if any are linked in the markdown
-  python_urls = extract_python_urls(content)
-  python_urls.each do |py_url|
-    external_python_content = fetch_python_file_content(py_url)
-    combined_content += external_python_content
-  end
-
-  # Calculate read time based on combined content
-  read_time = calculate_read_time(combined_content)
-  # Update the read_time in the front matter (if it exists)
-  new_content = content.gsub(/(read_time:\s*)\d+/, "\\1#{read_time}")
-  # If read_time was not found, append it to the front matter
-  unless new_content.match?(/^read_time:/)
-    new_content = new_content.sub(/^---/, "---\nread_time: #{read_time}\n")
-  end
-
-  # Write back to the file
-  File.write(file_path, new_content)
+  read_time = calculate_read_time(total_text)
+  puts "Estimated read time for #{File.basename(file_path)}: #{read_time} minute(s)"
 end
 
-
-# Function to extract the notebook URL from the markdown content
-def extract_notebook_url(content)
-  if content.match(/<div id="notebook-content">\s*(https?:\/\/\S+)/)
-    return $1  # Return the first captured group (the URL)
-  end
-  nil
+# Run for all markdown posts
+Dir.glob(File.expand_path("../../../_posts/*.markdown", __FILE__)).each do |file|
+  puts "Processing: #{file}"
+  process_markdown_file(file)
 end
-
-
-# Function to extract .py file URLs from the markdown content
-def extract_python_urls(content)
-  urls = []
-  content.scan(/<button data-url="(https?:\/\/\S+\.py)">/) do |match|
-    urls << match[0]
-  end
-  urls
-end
-
-
-# Main script execution
-puts "Starting read time calculation..."
-Dir.glob('_posts/*.markdown') do |file_path|
-  process_markdown_file(file_path)
-end
-puts "Read time calculation completed."
